2c475a8c84f731a94a46d1468fbf3521414d4946
John Harden <jharden@pnimedia.com>
2016-02-04 12:35:44 -0800

    tweaks to initial mockup

--- scripts/script.js
+++ scripts/script.js
@@ -7,29 +7,40 @@ window.onload = function(){
     small_click.addEventListener('click',setSmallTiles,false);
     medium_click.addEventListener('click',setMediumTiles,false);
     large_click.addEventListener('click',setLargeTiles,false);
+    var timeoutID;
 
+    function doSetTimeout(i,classname){
+
+        window.setTimeout(function(){
+
+            photos_list[i].className = classname;
+            //console.log('test: ' + i);
+        },200);
+    }
 
     function setSmallTiles(){
 
         for(var i = photos_list.length-1;i>=0; i--){
-            photos_list[i].className = "photo-tile column-six";
-        }
 
+           timeoutID = doSetTimeout(i,"photo-tile column-six");
+        }
     }
 
     function setMediumTiles(){
 
+
         for(var i = photos_list.length-1;i>=0; i--){
-            photos_list[i].className = "photo-tile column-four";
+
+            timeoutID = doSetTimeout(i,"photo-tile column-four");
         }
     }
 
     function setLargeTiles(){
 
         for(var i = photos_list.length-1;i>=0; i--){
-            photos_list[i].className = "photo-tile column-three";
+
+            timeoutID = doSetTimeout(i,"photo-tile column-three");
         }
     }
-
 
 };
