d81cba35f7156e310fba5241be16fe98ee790a0a
John Harden <jharden@pnimedia.com>
2016-02-09 17:23:12 -0800

    tidied up javascript - removed unused  functions

--- scripts/script.js
+++ scripts/script.js
@@ -8,105 +8,60 @@ window.onload = function(){
     medium_click.addEventListener('click',setMediumTiles,false);
     large_click.addEventListener('click',setLargeTiles,false);
     var small_thumb_list, med_thumb_list,large_thumb_list;
-    var isSmall = true,isMed =false,isLarge =false;
     //create 3 d2 arrays based and store data
 
-    function initsmall2dArray(){
+    function init2dArray(countPerRow,xdim,ydim){
 
-        small_thumb_list = new Array(document.getElementsByClassName('photo-tile').length);
-
-        console.log('init SMALL');
+        var localx = xdim;
+        var localy = ydim;
+        //init the new array
+        var arrayLength = photos_list.length;
+        var myList;
+        switch (countPerRow){
+            case 6:
+                small_thumb_list = new Array(document.getElementsByClassName('photo-tile').length);
+                myList = small_thumb_list;
+                break;
+            case 4:
+                med_thumb_list = new Array(document.getElementsByClassName('photo-tile').length);
+                myList = med_thumb_list;
+                break;
+            case 3:
+                large_thumb_list = new Array(document.getElementsByClassName('photo-tile').length);
+                myList = large_thumb_list;
+                break;
+        }
         //outer array contains number of rows
         var count = 0;
-        for(var i = 0; i<Math.ceil(small_thumb_list.length/6);i++){
+        for(var i = 0; i<Math.ceil(arrayLength/countPerRow);i++){
 
-           // console.log('row: ' + i);
-            small_thumb_list[i] = new Array(6);
+            myList[i] = new Array(countPerRow);
             //inner array will contain coords. j will break every 6 elements
-            for(var j =0; j<6;j++){
-
-                small_thumb_list[i][j] = {
-                    x: 0 + (190 *(j)),
-                    y: 0 + (150 *(i))
+            for(var j =0; j<countPerRow;j++){
+                myList[i][j] = {
+                    x: 0 + (localx *(j)),
+                    y: 0 + (localy *(i))
                 };
-                //console.log('coords: x: ' + +small_thumb_list[i][j].x + ' y: '+small_thumb_list[i][j].y);
                 count++;
-                if(count === small_thumb_list.length){
-
+                if(count === arrayLength){
                     break;
                 }
             }
         }
-        return small_thumb_list;
-    }
-
-    small_thumb_list = initsmall2dArray();
-
-    function initmedium2dArray(){
-        med_thumb_list =  new Array(document.getElementsByClassName('photo-tile').length);
-        console.log('init MEDIUM');
-        //outer array contains number of rows
-        var count = 0;
-        for(var i = 0; i<Math.ceil(med_thumb_list.length/4);i++){
-
-            //console.log('row: ' + i);
-            med_thumb_list[i] = new Array(4);
-            //inner array will contain coords. j will break every 6 elements
-            for(var j =0; j<4;j++){
-
-                med_thumb_list[i][j] = {
-                    x:  0 + (280 *(j)),
-                    y: 0 + (220 *(i))
-                };
-               // console.log('coords: x: ' + +med_thumb_list[i][j].x + ' y: '+med_thumb_list[i][j].y);
-                count++;
-                if(count === med_thumb_list.length){
-                    break;
-                }
-            }
+        switch (countPerRow){
+            case 6:
+                return small_thumb_list;
+            case 4:
+                return  med_thumb_list;
+            case 3:
+                return large_thumb_list;
         }
-        return med_thumb_list;
     }
-    med_thumb_list = initmedium2dArray();
-
-    function initlarge2dArray(){
-        large_thumb_list =  new Array(document.getElementsByClassName('photo-tile').length);
-        console.log('init LARGE');
-        //outer array contains number of rows
-        var count = 0;
-        for(var i = 0; i<Math.ceil(large_thumb_list.length/3);i++){
-
-            large_thumb_list[i] = new Array(3);
-            //inner array will contain coords. j will break every 6 elements
-            for(var j =0; j<3;j++){
-
-                large_thumb_list[i][j] = {
-                    x: 0 + (380 *(j)),
-                    y: 0 + (290 *(i))
-                };
-                count++;
-                if(count === large_thumb_list.length){
-                    break;
-                }
-            }
-        }
-        return large_thumb_list;
-    }
-    large_thumb_list = initlarge2dArray();
-
-   function getOffset( el ) {
-        var _x = 0;
-        var _y = 0;
-        while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-            _x += el.offsetLeft - el.scrollLeft;
-            _y += el.offsetTop - el.scrollTop;
-            el = el.offsetParent;
-        }
-        return { top: _y, left: _x };
-    }
+    small_thumb_list = init2dArray(6,190,150);
+    med_thumb_list = init2dArray(4,280,220);
+    large_thumb_list = init2dArray(3,380,290);
 
     function doTranslate(counter,array_length){
-
         var thumb_list;
         if(array_length === 6){
             thumb_list = small_thumb_list;
@@ -115,77 +70,51 @@ window.onload = function(){
         }else if(array_length === 3){
             thumb_list = large_thumb_list;
         }
-
         var dest_xcoord = (Math.ceil((counter+1)/array_length))-1;
         var dest_ycoord = 0;
-
         if(((counter+1)%array_length)===0){
             dest_ycoord = array_length-1;
         }else{
             dest_ycoord = ((counter+1)%array_length)-1;
         }
         var destination_coords = thumb_list[dest_xcoord][dest_ycoord];
-
         //do the thing
         photos_list[counter].style="transform:translate3d("+(destination_coords.x)+"px,"+(destination_coords.y)+"px,0);";
-
-
     }
 
     function setNewCoordinates(element,counter){
-
         //new classname;
         if(element.className === "photo-tile column-six"){
-
             doTranslate(counter,6);
-
         }else if(element.className === "photo-tile column-four"){
-
-            //if(isSmall){
-                doTranslate(counter,4);
-            //}
+            doTranslate(counter,4);
         }else if(element.className === "photo-tile column-three"){
             doTranslate(counter,3);
         }
     }
 
     function doIntervalClassChange(classname){
-
         var counter = photos_list.length-1;
         var i = setInterval(function(){
-
             photos_list[counter].className = classname;
-
             setNewCoordinates(photos_list[counter],counter);
             counter--;
             if(counter < 0) {
                 clearInterval(i);
             }
         }, 100);
-
     }
-
     function setSmallTiles(){
-
         doIntervalClassChange('photo-tile column-six');
-
     }
-
     function setMediumTiles(){
-
         doIntervalClassChange('photo-tile column-four');
-
     }
-
     function setLargeTiles(){
-
         doIntervalClassChange('photo-tile column-three');
     }
 
-
-
     function onLoadSort(){
-
         for(var i = photos_list.length-1;i>0;i--) {
             var counter = i;
             var origin_xcoord = (Math.ceil((counter + 1) / 6)) - 1;
